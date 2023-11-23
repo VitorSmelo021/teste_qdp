90a6815b05a0499588a7ad3f5d6f1b1f5ad26a5b
Vitor Samuel <108810741+VitorSmelo021@users.noreply.github.com>
2023-11-23 14:44:49 -0300

    Add files via upload

--- teste/modal_add.js
+++ teste/modal_add.js
@@ -1,5 +1,7 @@
 const modal_open = document.getElementById("modal_open")
 const modal_bg = document.getElementById("modal_bg")
+
+
 modal_open.addEventListener(()=>{
-    modal_bg.style.display = 'flex'
+    modal_bg.style.display = 'flex';
 })
